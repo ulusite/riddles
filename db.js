034691b744bd8c2afedc437e2b1b385193329891
ulusite <ulusite@gmail.com>
2024-02-22 10:05:07 -0800

    Add support for riddle question as a link

--- db.js
+++ db.js
@@ -1,7 +1,118 @@
 const db2024feb = [
     {
+        question: '正月十五為上元，是夜為元宵。七月十五為中元。是否有下元？',
+        choices: [
+            {
+                answer: '有。八月十五中秋亦稱下元',
+            },
+            {
+                answer: '有。臘月十五為下元'
+            },
+            {
+                answer: '有。十月十五為下元',
+                correct: true
+            },
+	        {
+		        answer: '沒有下元',
+            }
+        ]
+    },
+    {
+        // question: '皇',
+        question: '看圖猜題',
+        hint: '成語（4字）。比喻十全十美。',
+        imageId: 'char1',
+        choices: [
+            {
+                answer: ['白玉無瑕', '白玉无瑕'],
+            }
+        ]
+    },
+    {
+        question: '看圖組字',
+        hint: '初春開花植物 （2或3字）',
+        imageId: 'waterFall',
+        choices: [
+            {
+                answer: ['水仙', '水仙花'],
+            },
+        ]
+    },
+    {
+        question: '我欲乘風歸去，高處不再寒',
+        hint: '元宵節應景活動物品 （2字）',
+        choices: [
+            {
+                answer: ['天燈', '天灯'],
+            }
+        ]
+    },
+    {
+        question: '清風徐來 水波不興',
+        hint: '新北市觀光地名 （2字）',
+        choices: [
+            {
+                answer: '平溪',
+            }
+        ]
+    },
+    {
+        question: '美國西部有哪四個州，州界四角接連在一起？',
+        choices: [
+            {
+                answer: '加州, 奧勒岡, 內華達, 愛德荷',
+            },
+            {
+                answer: '奧勒岡，華盛頓，內華達，懷俄明',
+            },
+            {
+                answer: '內華達，亞利桑那，新墨西哥，科羅拉多',
+            },
+            {
+                answer: '亞利桑那，新墨西哥，科羅拉多，猶他',
+                correct: true,
+            },
+            {
+                answer: '加州， 亞利桑那， 新墨西哥， 科羅拉多',
+            }
+        ]
+    },
+    {
+        question: '下列哪個湖的名字，在英文字母中最先出現？',
+        choices: [
+            {
+                answer: '安大略湖',
+            },
+            {
+                answer: '密西根湖',
+            },
+            {
+                answer: '衣律湖',
+            },
+            {
+                answer: '太浩湖',
+            },
+            {
+                answer: '伊麗莎白湖',
+                correct: true
+            }
+        ]
+    },
+    {
+        // question: '一根棍子，吊著方箱；一把梯子，搭在中央',
+        question: '看圖猜題',
+        hint: '打一字。每人都有。丟掉的話，很糗',
+        imageId: 'char3',
+        choices: [
+            {
+                answer: '面',
+            }
+        ]
+    },
+    {
         question: '熱烘烘的太陽往上爬哟',
         hint: '台灣地名',
+        skip: true,
         choices: [
             {
                 answer: '春日',
@@ -26,34 +137,66 @@ const db2024feb = [
         ]
     },
     {
+        // question: '一邊是紅，一邊是綠。一邊喜風，一邊喜雨',
+        question: '看圖猜題',
+        hint: '打一字。在24節氣中此字出現2次',
+        imageId: 'char2',
+        choices: [
+            {
+                answer: '秋',
+		        notes: '禾：禾是水稻，水稻是綠色，喜歡雨水。火：火是红色，遇風則長'
+            }
+        ]
+    },
+    {
         question: '墾丁之北，爾濱之南',
         hint: '看江山如此多嬌，猜四時不謝之花',
         choices: [
             {
-                answer: '報春花',
+                answer: '百合花',
             },
             {
-                answer: '迎春花',
+                answer: '桔梗花',
             },
             {
                 answer: '日日春',
                 correct: true,
-                notes: '墾丁之北為恆春，爾濱之南為長春，皆日日春也'
+                notes: '墾丁之北為恆春，爾濱之南為長春，日日皆春'
             },
             {
-                answer: '仙草',
+                answer: '魯冰花',
             },
             {
                 answer: '以上皆非',
             },
         ]
     },
+
     {
-        question: '清風徐來 水波不興',
-        hint: '台灣觀光地名 （2字）',
+        question: '智齒',
+        hint: '中國傳統祭祀活動。近代在閩臺一帶盛行 （2字）',
         choices: [
             {
-                answer: '平溪',
+                answer: '尾牙'
+            }
+        ]
+    },
+    {
+        question: '立春',
+        hint: '一種音樂活動名 （2字）拆字格',
+        choices: [
+            {
+                answer: '合唱',
+                notes: '立春字解為三人立日，即众/眾【三人】音【立日】，合唱也'
+            }
+        ]
+    },
+    {
+        question: '2024美國總統大選的【柯文哲】',
+        hint: '人名。1. 在超級星期天足球大賽的廣告中出現過 2. 出生於美國政治世家 3. 伯父和父親都參加過美國總統競選',
+        choices: [
+            {
+                answer: ['robert kennedy jr', 'robert f. kennedy jr'],
             }
         ]
     },
@@ -86,6 +229,358 @@ const db2024feb = [
     },
     {
         question: ' ',
+        hint: '人際行爲 （2字）',
+        choices: [
+            {
+                answer: ['約會', '约会'],
+            }
+        ]
+    },
+    {
+        question: '百家爭鳴',
+        hint: '物理學名詞 （4字）',
+        choices: [
+            {
+                answer: ['量子糾纏', '量子纠缠']
+            }
+        ]
+    },
+    {
+        question: '下列哪幾行詩句是描寫的元宵情景?',
+        imageId: 'poems',
+        choices: [
+            {
+                answer: '1，3，4',
+            },
+	        {
+                answer: '1，2，3，4',
+            },
+            {
+                answer: '1，2，4',
+                correct: true
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '竹杖芒鞋輕勝馬，一蓑煙雨任平生',
+        hint: '小説人物',
+        skip: true,
+        choices: [
+            {
+                answer: '賈寶玉',
+            },
+            {
+                answer: '令狐冲',
+            },
+            {
+                answer: '林冲',
+            },
+            {
+                answer: ['魯達', '魯智深', '鲁达', '鲁智深'],
+                correct: true,
+                notes: '清，邱圓《魯智深醉鬧五台山》《寄生草·漫揾英雄淚》：煙蓑雨笠卷單行，一任俺，芒鞋破缽隨緣化'
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '跑旱船',
+        hint: '南宋著名詩人名 （2字）',
+        choices: [
+            {
+                answer: ['陸游', '陸放翁', '陆游', '陆放翁', '放翁'],
+            }
+        ]
+    },
+    {
+        question: '衣錦還鄉',
+        hint: '明朝文人名，擅作抒情文 （3字）',
+        choices: [
+            {
+                answer: ['歸有光', '归有光'],
+            }
+        ]
+    },
+    {
+        question: '老子曰',
+        hint: '唐朝著名詩人名 （2字）',
+        choices: [
+            {
+                answer: ['李白', '李太白'],
+		        notes: '老子姓李名耳，老子曰即李白'
+            }
+        ]
+    },
+    {
+        question: '子曰',
+        hint: '書名（不是論語， 2字）',
+        choices: [
+            {
+                answer: ['聖經', '圣经'],
+		        notes: '孔子聖人，聖人説話即聖經'
+            }
+        ]
+    },
+    {
+        question: '辛棄疾《青玉案·元夕》：[鳳簫聲動，玉壺光轉] 玩的是什麽燈？',
+        skip: true,
+        choices: [
+            {
+                answer: '天燈',
+            },
+            {
+                answer: '龍燈',
+                correct: true,
+                notes: '龍燈或魚燈，[鳳簫聲動，玉壺光轉，一夜魚龍舞]'
+            },
+            {
+                answer: ['魚燈', '龍燈'],
+                correct: true,
+                notes: '龍燈或魚燈，[鳳簫聲動，玉壺光轉，一夜魚龍舞]'
+            },
+	        {
+		        answer: '水燈',
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '胖子泡溫泉',
+        hint: '應時食品 （2字）',
+        choices: [
+            {
+                answer: ['湯圓', '汤圆'],
+            }
+        ]
+    },
+    {
+        question: '川味髮型',
+        hint: '食品。生長在西南，茁壯於東北。最近流行遍灣區 （3字）',
+        choices: [
+            {
+                answer: ['麻辣燙', '麻辣烫'],
+            }
+        ]
+    },
+    {
+        question: '少林寺運動會',
+        hint: '年節菜名 （3字）',
+        choices: [
+            {
+                answer: ['佛跳墻', '佛跳墙']
+            }
+        ]
+    },
+    {
+        question: '剖腹生產',
+        hint: '物理學/化學名詞 （2字）',
+        choices: [
+            {
+                answer: ['分子']
+            }
+        ]
+    },
+    {
+        question: '親生的',
+        hint: '物理學/化學名詞 （2字）',
+        choices: [
+            {
+                answer: ['原子']
+            }
+        ]
+    },
+    {
+        question: '呱呱墜地',
+        hint: '物理學/化學名詞 （2字）',
+        choices: [
+            {
+                answer: ['光子']
+            }
+        ]
+    },
+    {
+        question: '樓閣玲瓏五雲起，一騎紅塵妃子笑',
+        hint: '城市',
+        skip: true,
+        choices: [
+            {
+                answer: '成都',
+                notes: '四川'
+            },
+            {
+                answer: '重慶',
+                correct: true
+            },
+            {
+                answer: '莆田',
+                notes: '福建'
+            },
+            {
+                answer: '增城',
+                notes: '廣東'
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '小樓一夜聽春雨，賭書潑得永日香',
+        hint: '美食家多愛',
+        skip: true,
+        choices: [
+            {
+                answer: '酒'
+            },
+            {
+                answer: '茶',
+                correct: true,
+                notes: '放翁【晴窗細乳戲分茶】，易安《金石錄後序》賭書潑茶'
+            },
+            {
+                answer: '筍',
+            },
+            {
+                answer: '河豚',
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+];
+
+const db2024 = [
+    {
+        question: '油管聼歌猜題',
+        link: 'https://www.youtube.com/watch?v=ijOZRKla6yg',
+        hint: '台灣地名',
+        choices: [
+            {
+                answer: '春日',
+                notes: '屏東'
+            },
+            {
+                answer: '烏日',
+                notes: '台中'
+            },
+            {
+                answer: '佳冬',
+                notes: '屏東'
+            },
+            {
+                answer: '暖暖',
+                correct: true,
+                notes: '基隆'
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '墾丁之北，爾濱之南',
+        hint: '看江山如此多嬌，猜四時不謝之花',
+        choices: [
+            {
+                answer: '百合花',
+            },
+            {
+                answer: '桔梗花',
+            },
+            {
+                answer: '日日春',
+                correct: true,
+                notes: '墾丁之北為恆春，爾濱之南為長春，日日皆春'
+            },
+            {
+                answer: '魯冰花',
+            },
+            {
+                answer: '以上皆非',
+            },
+        ]
+    },
+    {
+        question: '墾丁之北，爾濱之南',
+        hint: '看江山如此多嬌，猜四時不謝之花',
+        skip: true,
+        choices: [
+            {
+                answer: '報春花',
+            },
+            {
+                answer: '迎春花',
+            },
+            {
+                answer: '日日春',
+                correct: true,
+                notes: '墾丁之北為恆春，爾濱之南為長春，皆日日春也'
+            },
+            {
+                answer: '仙草',
+            },
+            {
+                answer: '以上皆非',
+            },
+        ]
+    },
+    {
+        question: '清風徐來 水波不興',
+        hint: '台灣觀光地名 （2字）',
+        choices: [
+            {
+                answer: '平溪',
+            }
+        ]
+    },
+    {
+        question: '白髮三千丈，會須一飲三百杯',
+        hint: '數學名詞 ',
+        textBox: true,
+        choices: [
+            {
+                answer: '虛數',
+            },
+            {
+                answer: '複數'
+            },
+            {
+                answer: ['無理數', '无理数'],
+                correct: true
+            },
+            {
+                answer: '循環數',
+            },
+        ]
+    },
+    {
+        question: '開會',
+        hint: '物理學名詞 （3字）',
+        choices: [
+            {
+                answer: ['相對論', '相对论'],
+            }
+        ]
+    },
+    {
+        question: '開大會',
+        hint: '物理學名詞 （5字）',
+        choices: [
+            {
+                answer: ['廣義相對論', '广义相对论'],
+            }
+        ]
+    },
+    {
+        question: '狹義相對論 ',
         hint: '人際行爲 （2字）',
         choices: [
             {
@@ -140,20 +635,59 @@ const db2024feb = [
         ]
     },
     {
-        question: '古時女子鮮少外出抛頭露面。只有元宵前後三五夜，她們可以自由的和閨蜜結伴出游嬉玩。看花賞燈之外，偶爾也遇到邂逅異性的機緣。下列哪些詩句是描寫的元宵情景?金吾不禁夜，花市燈如晝，月上柳梢頭，人約黃昏後，只見桃花不見人，桃花潭水深千尺，驀然回首 那人正在燈火闌珊，水中沚',
+        question: '下列哪幾行詩句是描寫的元宵情景?',
+        imageId: 'poems',
         choices: [
             {
-                answer: '2,3,5',
+                answer: '1，3，4',
             },
 	        {
-                answer: '1,2,6',
+                answer: '1，2，3，4',
             },
             {
-                answer: '1,2,5',
+                answer: '1，2，4',
                 correct: true
             },
             {
                 answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '老子曰',
+        hint: '唐朝著名詩人 （2字）',
+        choices: [
+            {
+                answer: ['李白', '李太白'],
+		        notes: '老子姓李名耳，老子曰即李白'
+            }
+        ]
+    },
+    {
+        question: '子曰',
+        hint: '書名（不是論語， 2字）',
+        choices: [
+            {
+                answer: ['聖經', '圣经'],
+		        notes: '孔子聖人，聖人説話即聖經'
+            }
+        ]
+    },
+    {
+        question: '跑旱船',
+        hint: '南宋著名詩人 （2字）',
+        choices: [
+            {
+                answer: ['陸游', '陸放翁', '陆游', '陆放翁', '放翁'],
+            }
+        ]
+    },
+    {
+        question: '衣錦還鄉',
+        hint: '明朝文人，擅作抒情文 （3字）',
+        choices: [
+            {
+                answer: ['歸有光', '归有光'],
             }
         ]
     },
@@ -177,44 +711,6 @@ const db2024feb = [
             },
             {
                 answer: '以上皆非',
-            }
-        ]
-    },
-    {
-        question: '跑旱船',
-        hint: '南宋著名詩人 （2字）',
-        choices: [
-            {
-                answer: ['陸游', '陸放翁', '陆游', '陆放翁', '放翁'],
-            }
-        ]
-    },
-    {
-        question: '衣錦還鄉',
-        hint: '明朝文人，擅作抒情文 （3字）',
-        choices: [
-            {
-                answer: ['歸有光', '归有光'],
-            }
-        ]
-    },
-    {
-        question: '老子曰',
-        hint: '唐朝著名詩人 （2字）',
-        choices: [
-            {
-                answer: ['李白', '李太白'],
-		        notes: '老子姓李名耳，老子曰即李白'
-            }
-        ]
-    },
-    {
-        question: '子曰',
-        hint: '書名（不是論語， 2字）',
-        choices: [
-            {
-                answer: ['聖經', '圣经'],
-		        notes: '孔子聖人，聖人説話即聖經'
             }
         ]
     },
@@ -243,9 +739,57 @@ const db2024feb = [
         ]
     },
     {
-        question: '看圖猜謎',
-        hint: '初春植物 （2或3字）',
-        imageId: 'photo1',
+        question: '樓閣玲瓏五雲起，一騎紅塵妃子笑',
+        hint: '城市',
+        choices: [
+            {
+                answer: '成都',
+                notes: '四川'
+            },
+            {
+                answer: '重慶',
+                correct: true
+            },
+            {
+                answer: '莆田',
+                notes: '福建'
+            },
+            {
+                answer: '增城',
+                notes: '廣東'
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '小樓一夜聽春雨，賭書潑得永日香',
+        hint: '美食家多愛',
+        choices: [
+            {
+                answer: '酒'
+            },
+            {
+                answer: '茶',
+                correct: true,
+                notes: '放翁【晴窗細乳戲分茶】，易安《金石錄後序》賭書潑茶'
+            },
+            {
+                answer: '筍',
+            },
+            {
+                answer: '河豚',
+            },
+            {
+                answer: '以上皆非',
+            }
+        ]
+    },
+    {
+        question: '看圖組字',
+        hint: '初春開花植物 （2或3字）',
+        imageId: 'waterFall',
         choices: [
             {
                 answer: ['水仙', '水仙花'],
@@ -255,6 +799,7 @@ const db2024feb = [
     {
         question: '我欲乘風歸去，高處不再寒',
         hint: '元宵節應景活動所用物品 （2字）',
+        skip: true,
         choices: [
             {
                 answer: ['天燈', '天灯'],
@@ -289,52 +834,29 @@ const db2024feb = [
         ]
     },
     {
-        question: '樓閣玲瓏五雲起，一騎紅塵妃子笑',
-        hint: '城市',
-        skip: true,
+        question: '剖腹生產',
+        hint: '物理學/化學名詞 （2字）',
         choices: [
             {
-                answer: '成都',
-                notes: '四川'
-            },
-            {
-                answer: '重慶',
-                correct: true
-            },
-            {
-                answer: '莆田',
-                notes: '福建'
-            },
-            {
-                answer: '增城',
-                notes: '廣東'
-            },
-            {
-                answer: '以上皆非',
+                answer: ['分子']
             }
         ]
     },
     {
-        question: '小樓一夜聽春雨，賭書潑得永日香',
-        hint: '美食家多愛',
-        skip: true,
+        question: '親生的',
+        hint: '物理學/化學名詞 （2字）',
         choices: [
             {
-                answer: '酒'
-            },
+                answer: ['原子']
+            }
+        ]
+    },
+    {
+        question: '呱呱墜地',
+        hint: '物理學/化學名詞 （2字）',
+        choices: [
             {
-                answer: '茶',
-                correct: true,
-                notes: '放翁【晴窗細乳戲分茶】，易安《金石錄後序》賭書潑茶'
-            },
-            {
-                answer: '筍',
-            },
-            {
-                answer: '河豚',
-            },
-            {
-                answer: '以上皆非',
+                answer: ['光子']
             }
         ]
     },
@@ -513,6 +1035,10 @@ const riddlesDB = {
     '2024feb': {
         title: '2024 甲辰龍年',
         db: db2024feb
+    },
+    '2024': {
+        title: '2024 甲辰龍年',
+        db: db2024
     },
     '2019': {
         title: '2019 己亥豬年',
